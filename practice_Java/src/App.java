ee1dbca454ad3b90caf94f5008513fa39e2f8987
hiroaki <hiroaki2001519@docomo.ne.jp>
2026-04-14 22:37:07 +0900

    UserServiceの実装、getAverageAge, getTotalPoint, findByNameの実装

--- practice_Java/src/App.java
+++ practice_Java/src/App.java
@@ -1,4 +1,5 @@
 import java.util.List;
+import java.util.ArrayList;
 
 
 class User {
@@ -12,24 +13,63 @@ class User {
     }
 }
 
+
 class UserService {
     List<User> users;
+
     UserService(List<User> users) {
         this.users = users;
     }
 
+    public double getAverageAge() {
+        double sum = 0;
+        for (User user : users) {
+            sum += user.age;
+        }
+        return sum / users.size();
+    }
 
+    public int getTotalPoint() {
+        int sum = 0;
+        for (User user : users) {
+            sum += user.point;
+        }
+        return sum;
+    }
+
+    public boolean findByName(String name) {
+        System.out.println(name + "の検索の結果・・・");
+        boolean flag = false;
+        for (User user : users) {
+            if (user.name.equals(name)) {
+                return flag = true;
+            }
+        }
+        return flag;
+    }
 }
 
 public class App {
 
-    
+    public static List<User> createUser() {
+        List<User> users = new ArrayList<>();
+
+        users.add(new User("hiroaki", 80, 24));
+        users.add(new User("yoshida", 60, 22));
+        users.add(new User("hoge", 70, 36));
+
+        return users;
+    }
+
+
+
 
     public static void main(String[] args) throws Exception {
-        User user = new User("hiroaki", 90, 24);
-        System.out.println(user);
-        System.out.println(user.name);
-        System.out.println(user.age);
-        System.out.println(user.point);
+        List<User> users = createUser();
+        UserService service = new UserService(users);
+        System.out.println("service " + service);
+        System.out.println(service.getAverageAge());
+        System.out.println(service.getTotalPoint());
+        System.out.println(service.findByName("hiroaki"));
     }
 }
